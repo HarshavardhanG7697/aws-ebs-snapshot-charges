33dc2d1bb635ce08680996561a5be42607e3254e
HarshavardhanG7697 <harsha7697@gmail.com>
2022-09-12 09:00:01 +0000

    Successfully implemented the function to caluclate the age of snapshot
    * Used the round() to round up to 2 figures after decimal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from asyncio.log import logger
 import boto3
 import logging
+import datetime
+from dateutil.tz import tzlocal
 
 logging.basicConfig(
     level=logging.INFO, format="pid=%(process)d[%(levelname)s]:%(message)s"
@@ -28,15 +30,32 @@ def get_ebs_snapshots(volume_id):
     return snapshots_list["Snapshots"]
 
 
+def get_snapshot_id(snapshot):
+    return snapshot["SnapshotId"]
+
+
 def get_snapshot_name(snapshot):
+    logger.info(f"Retrieving snapshot name for snapshot: {snapshot['SnapshotId']}")
     for tag in snapshot["Tags"]:
         if tag["Key"] == "Name":
+            logger.info(f"Name Tag present for snapshot: {snapshot['SnapshotId']}")
             return tag["Value"]
-    raise Exception(
-        f"{snapshot['SnapshotId']} does not have a Name tag.\n \
-        But it has the following tags: {snapshot['Tags']}"
-    )
+    return None
 
+
+def time_since_snapshot(snapshot):
+    logger.info(f"Calculating age of snapshot: {snapshot['SnapshotId']}")
+    snapshot_created = snapshot["StartTime"]
+    time_now = datetime.datetime.now(tzlocal())
+    logger.info(f"Current time: {time_now}")
+    try:
+        hours = round(((time_now - snapshot_created).total_seconds()) / 3600, 2)
+        return hours
+    except Exception as e:
+        logger.error(f"Error while calculating age of snapshot: {snapshot['SnapshotId']}")
+        logger.error(e)
+        return None
+    
 
 volumes = ["vol-029cbf276b7f62071", "vol-099624baeb6ddac32"]
 logger.info(f"Looking for snapshots of volumes: {volumes}")
@@ -46,9 +65,17 @@ for volume in volumes:
         logger.warning(f"There are no snapshots found for volume: {volume}")
     else:
         for snapshot in snapshots:
-            logger.info(f"Snapshot ID: {snapshot['SnapshotId']}")
-            logger.info(f"{snapshot['SnapshotId']} created on: {snapshot['StartTime']}")
-            try:
-                logger.info(f"Name of the snapshot: {get_snapshot_name(snapshot)}")
-            except Exception as e:
-                logger.error(e)
+            snapshot_id = get_snapshot_id(snapshot)
+            logger.info(f"Snapshot id: {snapshot_id}")
+            snapshot_name = get_snapshot_name(snapshot)
+            if snapshot_name:
+                logger.info(f"Snapshot name: {snapshot_name}")
+            else:
+                logger.warning(
+                    f"{snapshot['SnapshotId']} does not have a Name tag.\nBut it has the following tags: {snapshot['Tags']}"
+                )
+            snapshot_age = time_since_snapshot(snapshot)
+            if not snapshot_age:
+                logger.warning(f"Unable to calculate the age of snapshot: {snapshot['SnapshotId']}")
+            else:
+                logger.info(f"Age of {snapshot_id}: {snapshot_age} hours")
